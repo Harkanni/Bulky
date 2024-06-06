cc1b9d0375d13dbda0e342602b6cc55b1a1c6c75
harkanni <harkanzyempire001@gmail.com>
2024-06-06 11:10:32 +0100

    Product model added

---
+++ BulkyModels/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class Product
+    {
+        [Key]
+        public int PId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+    }
+}
